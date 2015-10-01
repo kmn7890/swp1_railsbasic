0a79cf2272fb6a92b1fc7562865bce63602762fb
kmn7890 <kmn7890@gmail.com>
2015-10-01 06:31:34 +0000

    assignment

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,16 @@
+class HomeController < ApplicationController
+    def index
+    end
+    
+    def about
+    end
+    
+      def page1
+    end
+    
+      def page2
+    end
+    
+      def page3 
+     end
+end
